66e8cb5e7fc7e8d38502711c0b84fc86518bcdf1
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-08-12 17:16:10 +0530

    cancelled spelling fix

--- refactor-early-returns-1/index.js
+++ refactor-early-returns-1/index.js
@@ -1,7 +1,7 @@
 /**
  * Processes an order based on its payment and shipping status.
  *
- * @param {{isPaid: boolean, isShipped: boolean, isCancelled: boolean}} order - An object representing the order, with boolean properties 'isPaid', 'isShipped', and 'isCancelled'.
+ * @param {{isPaid: boolean, isShipped?: boolean, isCanceled?: boolean}} order - An object representing the order, with boolean properties 'isPaid', 'isShipped', and 'isCancelled'.
  * @return {string} A message describing the order's status.
  */
 export function processOrder(order) {
@@ -9,10 +9,10 @@ export function processOrder(order) {
 
   if (order.isPaid) {
     if (order.isShipped) {
-      result = 'Order is already shipped.';
+      result = 'Order is shipped.';
     } else {
-      if (order.isCancelled) {
-        result = 'Order is cancelled.';
+      if (order.isCanceled) {
+        result = 'Order is canceled.';
       } else {
         result = 'Order is paid but not shipped.';
       }
