f786c7cc925b0bc68c23c269d6e70a7b41f936fa
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-08-13 13:48:30 +0530

    feat: js docs

--- polyfill-array-at/index.js
+++ polyfill-array-at/index.js
@@ -9,4 +9,10 @@ if (!Array.prototype.at) {
 // However, for this exercise, we'll define it with a different name to not confuse the global object.
 // We can of course, also override it but let's not do that!
 
+/**
+ * Returns the array element at the specified index.
+ *
+ * @param {number} index - The index of the element to return.
+ * @return {*} The element at the specified index, or undefined if the index is out of bounds.
+ */
 export function myAt(index) {}
