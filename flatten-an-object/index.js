a9bdc56c3b521f5dcebec2e4a1229cf5211cf089
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-08-18 15:26:14 +0530

    feat: question init

---
+++ flatten-an-object/index.js
@@ -0,0 +1,9 @@
+/**
+ * Flattens a nested object into a single-level object.
+ *
+ * @param {Object} obj - The object to be flattened.
+ * @return {Object} The flattened object.
+ */
+export function flattenObject(obj) {
+  // Implementation goes here.
+}
---
+++ flatten-an-object/index.test.js
@@ -0,0 +1,5 @@
+import { describe, test } from 'vitest';
+
+describe('flatten-an-object', () => {
+  test.todo('', () => {});
+});
